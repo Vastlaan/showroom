a95f8d50d1052cc73fdbc3fae51b1ead9f0a1949
vastlaan <vastlaan@mail.com>
2020-11-13 20:03:28 +0100

    Improve navigation

--- client/src/components/navigation2/index.jsx
+++ client/src/components/navigation2/index.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import styled from 'styled-components'
 import Logo1 from '../logo1'
 import {respond, fonts, ButtonFull} from '../../styles'
+import {CgMenuMotion} from 'react-icons/cg'
+
 
 export default function Navigation2() {
+
+  const [isVisible, setIsVisible] = useState(false)
+
   return (
     <Container>
+      <Top>
+        <Logo1/>
+        <Icon onClick={()=>setIsVisible(prevState=>!prevState)}>
+          <CgMenuMotion/>
+        </Icon>
+      </Top>
       
-      <Logo1/>
-      <Buttons>
+      <Buttons visible={isVisible}>
         <Link to='/'><ButtonPlain>Diensten</ButtonPlain></Link>
         <Link to='/'><ButtonPlain>Bedrijf</ButtonPlain></Link>
         <Link to='/'><ButtonPlain>Over Ons</ButtonPlain></Link>
@@ -22,19 +32,48 @@ export default function Navigation2() {
 
 const Container = styled.nav`
   width: 100%;
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+`
+const Top = styled.div`
   padding: 1.3rem;
+  width: 100%;
+  position: static;
+  z-index: 11;
+  background-color: ${p=>p.theme.primary2};
   display: flex;
   justify-content: space-between;
   align-items: center;
-  ${()=>respond('l','padding: 2.3rem 6.8rem;')}
-  
+  ${()=>respond('xl','padding: 2.3rem 0 0 6.8rem; width: auto;')}
 `
-
 const Buttons = styled.div`
   display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex: 1;
+  
+  transition: all .3s;
+  transform: ${p=>p.visible?'translateY(0%)':'translateY(-200%)'};
+
+  position: absolute;
+  top:100%;
+  left: 0;
+  flex-direction: column;
+  background-color: ${p=>p.theme.primary2};
+  width:100%;
+  align-items: flex-start;
+  padding: 2.7rem;
+  z-index: 10;
+
+
+
+  ${()=>respond('xl',`
+    transform: translateY(0%); 
+    position: static;
+    align-items: center;
+    justify-content: space-between;
+    width: 55%;
+    flex-direction: row;
+    padding: 2.3rem 6.8rem 0 6.8rem;
+  `)}
 
   a{
     text-decoration: none;
@@ -44,6 +83,7 @@ const ButtonMain = styled(ButtonFull)`
   background-color: rgba(255,255,255,.3);
   color: ${p=>p.theme.grey7};
   font-family: ${fonts.heading4};
+  margin: 1rem 0;
 `
 const ButtonPlain = styled.button`
   background-color: transparent;
@@ -54,9 +94,10 @@ const ButtonPlain = styled.button`
   color: ${p=>p.theme.grey7};
   cursor: pointer;
   transition: all .3s;
-  display: none;
+  margin: 1rem 0;
+  
 
-  ${()=>respond('xl','display: block;')}
+  
 
   &:hover{
     color: ${p=>p.theme.grey6};
@@ -65,4 +106,25 @@ const ButtonPlain = styled.button`
   &:active, :focus{
     outline: none;
   }
+`
+
+const Icon = styled.button`
+
+  border: none;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:focus, :active{
+    outline: none;
+  }
+
+  svg{
+    font-size: 5rem;
+    color: snow;
+  }
+  ${()=>respond('xl','display: none;')}
+
+
 `
